5a200ace6c455f2dc4d99843f512b52cc0a067a6
Nir Pochter <nir@lightricks.com>
2015-01-26 11:43:22 +0200

    P. 84.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -34,8 +34,11 @@ class NewVisiterTest(LiveServerTestCase):
 		# He types 'Buy peacock feathers'
 		inputbox.send_keys('Buy peacock feathers')
 
-		#Upon hitting enter, the page updates and lists the first todo item in the list.
+		#Upon hitting enter, she is taken to a new URL., and the page updates and lists the first 
+		#todo item in the list.
 		inputbox.send_keys(Keys.ENTER)
+		edith_list_url = self.browser.current_url
+		self.assertRegex(edith_list_url, '/lists/.+')
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		# The user adds a second item.
@@ -46,9 +49,29 @@ class NewVisiterTest(LiveServerTestCase):
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
+		# A new user enters.
+		## We use a new browser session to make sure data from previous user is not coming from cookie.
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
 
+		# User visits webpage, TODO list of old user is not shown.
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text)
 
-		# Need to add more tests. 		# He is invited to enter todo entries.
+		# He adds a new list.
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
 
-		self.fail()
+		# He gets his own URL.
+		francis_list_url = self.browser.current_url()
+		self.assertRegex(francis_list_url, '/lists/.+')
+		self.assertNotEqual(francis_list_url, edith_list_url)
+
+		# No sign of first user's list.
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertIn('Buy milk', page_text)
 		
